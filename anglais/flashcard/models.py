9f2f22d892ebf9e7cf8fb2f6500d78d492b3b70d
trungviet17 <113108053+trungviet17@users.noreply.github.com>
2024-03-24 12:41:59 +0700

    - thêm các models : 1. Folder : tập chứa studyset 2. Studyset : là tập từ vựng 3. Word : chứa thông tin từ vừng gồm giải nghĩa và từ ngôn ngữ gốc

--- anglais/flashcard/models.py
+++ anglais/flashcard/models.py
@@ -1,3 +1,50 @@
 from django.db import models
-
+from django.contrib.auth import User
+from django.utils import timezone 
 # Create your models here.
+
+
+
+class Folders(models.Model): 
+    """Class dùng để biểu diễn đối tượng folder cho toàn bộ file """
+
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    author = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'folders') #author of the folder
+    
+    publish = models.DateTimeField(default = timezone.now())
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self): 
+        return self.title 
+    
+
+class StudySet(models.Model): 
+    """Class dùng để biểu diễn tập từ vựng, nằm trong tập folder"""
+
+    title = models.CharField(max_length = 200)
+    description = models.TextField()
+    folder = models.ForeignKey(Folders, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    learning_time = models.IntegerField(default = 0)
+
+    def __str__(self):  
+        return f"{self.folder} - {self.title}" 
+    
+class Word(models): 
+    """Class này dùng để biểu diễn """
+    class  TypeOfWord(models.TextChoices): 
+        ENGLISH = 'en'
+        VIETNAMESE = 'vi'
+
+
+    studyset = models.ForeignKey(StudySet, on_delete=models.CASCADE)
+    ori_word = models.CharField(max_length = 30)
+    trans_word = models.CharField(max_length = 150)
+
+    or_lan = models.CharField(max_length = 3, choices = TypeOfWord.choices, default = TypeOfWord.ENGLISH)
+    ne_lan = models.CharField(max_length = 3, choices = TypeOfWord.choices, default = TypeOfWord.VIETNAMESE)
+    studyset = models.ForeignKey(StudySet, on_delete = models.CASCADE)
+
+
+
