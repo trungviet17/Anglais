6fe29da3bb73cd7183a073244e4c555dbbd0e0ab
trungviet17 <113108053+trungviet17@users.noreply.github.com>
2024-05-04 23:44:55 +0700

    update edit word

--- anglais/flashcard/urls.py
+++ anglais/flashcard/urls.py
@@ -3,19 +3,20 @@ from django.urls import path
 from . import views
 from django.conf import settings 
 from django.conf.urls.static import static
+from .views import StudySetWordUpdateView
 
 
 app_name = 'flashcard'
 
 urlpatterns = [
     # path('', views.home_view, name = 'home'),
-    path('', views.flash_card_home_view , name = "flash_card_home_view"),
+    # path('', views.flash_card_home_view , name = "flash_card_home_view"),
     
     # tạo ra một studyset mới 
     path('new_studyset/', views.new_studyset, name="new_studyset"), 
-    path('update/<int:id>' , views.update_studyset, name="update_studyset"), 
+    path('update/<int:pk>' , StudySetWordUpdateView.as_view(), name="update_studyset"), 
     path('delete-word/<int:form_id>/<int:word_id>', views.delete_word, name = 'delete_word'), 
-    path('studysets', views.studysets, name="studysets"),
+    path('', views.studysets, name="studysets"),
     path('new_word/<int:id>',views.new_word, name = "new_word"),
     path('delete-studyset/<int:id>', views.delete_studyset, name= "delete_studyset"), 
     path('edit-word/<int:form_id>/<int:word_id>', views.edit_word, name="edit_word"), 
