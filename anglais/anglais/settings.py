fdf7bf6716649141d9761b5cf40e4e5dac906f96
trungviet17 <113108053+trungviet17@users.noreply.github.com>
2024-05-13 20:58:11 +0700

    remove social-auth

--- anglais/anglais/settings.py
+++ anglais/anglais/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'bootstrap4',
     'django_htmx',
 
-    'social_django', 
+    
     'django_extensions',
 ]
 
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'anglais_main',
         'USER': 'root',
-        'PASSWORD' : '261223', 
+        'PASSWORD' : '17012004', 
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
@@ -156,22 +156,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTHENTICATION_BACKENDS = [
  'django.contrib.auth.backends.ModelBackend',
  'account.authentication.EmailAuthBackend',
- 'social_core.backends.facebook.FacebookOAuth2',
- 'social_core.backends.google.GoogleOAuth2',
-]
-
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.getenv('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY')
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.getenv('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET')
-
-SOCIAL_AUTH_PIPELINE = [
- 'social_core.pipeline.social_auth.social_details',
- 'social_core.pipeline.social_auth.social_uid',
- 'social_core.pipeline.social_auth.auth_allowed',
- 'social_core.pipeline.social_auth.social_user',
- 'social_core.pipeline.user.get_username',
- 'social_core.pipeline.user.create_user',
- 'account.authentication.create_profile',
- 'social_core.pipeline.social_auth.associate_user',
- 'social_core.pipeline.social_auth.load_extra_data',
- 'social_core.pipeline.user.user_details',
 ]
